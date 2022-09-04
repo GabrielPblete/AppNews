e7c97eed58374d6858991d10954e8b14b5027c67
GabrielPblete <gabriel.alonso.g97@gmail.com>
2022-09-03 20:29:38 -0400

    AppTerminada

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Article } from 'src/app/interfaces';
 import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
-
+import { ActionSheetController } from '@ionic/angular';
+import { Share } from '@capacitor/share';  
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-article',
@@ -13,7 +15,11 @@ export class ArticleComponent implements OnInit {
   @Input() article:Article
   @Input() index:number
 
-  constructor(private iab: InAppBrowser) { }
+  constructor(
+    private iab: InAppBrowser, 
+    private actionSheetCtrl:ActionSheetController,
+    private storageService:StorageService
+    ) { }
 
   ngOnInit() {}
 
@@ -21,6 +27,50 @@ export class ArticleComponent implements OnInit {
     const browser = this.iab.create(this.article.url);
     browser.show();
     //window.open(this.article.url, '_blank');
+
+  }
+  async openMenu(){
+
+    const articleInFavorites = this.storageService.articlesInFavorites(this.article);
+
+    const actionSheet = await this.actionSheetCtrl.create({
+      header : 'options',
+      buttons : [
+        {
+          text: 'Share',
+          icon: 'share-outline',
+          handler : ()=>this.shareArticle()
+        },
+        {
+          text: articleInFavorites ? 'Remove' : 'Favorites', 
+          icon: articleInFavorites ? 'heart' : 'heart-outline',
+          handler : ()=>this.onToggleFavorite()
+        },
+        {
+          text: 'Cancel',
+          icon: 'close-outline',
+          role : 'cancel'
+        }
+      ]
+
+    });
+
+    await actionSheet.present();
+
+  }
+
+  async shareArticle(){
+    console.log('share article');
+    await Share.share({
+      title: this.article.title,
+      text: this.article.source.name,
+      url: this.article.url
+    });
+  }
+
+  onToggleFavorite(){
+    console.log('Toogle article');
+    this.storageService.saveOrRemoveArticle(this.article);
   }
 
 }
